3d54ab04518717120f4cdfeaa011e065c175b40a
Edwin Sutanudjaja <sutanudjaja@edwin1.geo.uu.nl>
2015-03-30 13:42:55 +0200

    For analyzing natural discharge results at 30 arcmin resolution.

--- 0_main_analyze_discharge.py
+++ 0_main_analyze_discharge.py
@@ -16,12 +16,12 @@ logger = logging.getLogger("main_script")
 
 # PCR-GLOBWB results: model output directory, 
 pcrglobwb_output = {}
-pcrglobwb_output["folder"]               = "/projects/wtrcycle/users/edwinhs/05min_runs_28_november_2014/multi_cores_non_natural_1960_to_2010/global/" # "/scratch/edwin/05_arcmin_runs/2014_11_28/multi_cores_non_natural_1960_to_2010/global/" # "/scratch/edwin/IWMI_run_20_nov/without_fossil_limit_with_pumping_limit_CRU/netcdf/"
+pcrglobwb_output["folder"]               = "/scratch-shared/edwinhs/05min_runs_27_march_2015/multi_cores_natural/global/netcdf/" # "/projects/wtrcycle/users/edwinhs/05min_runs_28_november_2014/multi_cores_non_natural_1960_to_2010/global/" # "/scratch/edwin/05_arcmin_runs/2014_11_28/multi_cores_non_natural_1960_to_2010/global/" # "/scratch/edwin/IWMI_run_20_nov/without_fossil_limit_with_pumping_limit_CRU/netcdf/"
 pcrglobwb_output["netcdf_file_name"]     = "discharge_monthAvg_output.nc" 
 pcrglobwb_output["netcdf_variable_name"] = "discharge" 
 
 # output directory storing analysis results (results from this script)
-globalAnalysisOutputDir = "/projects/wtrcycle/users/edwinhs/05min_runs_28_november_2014/multi_cores_non_natural_1960_to_2010/analysis/monthly_discharge/" # "/scratch/edwin/IWMI_run_20_nov/without_fossil_limit_with_pumping_limit_CRU/analysis/monthly_discharge/"
+globalAnalysisOutputDir = "/scratch-shared/edwinhs/05min_runs_27_march_2015/multi_cores_natural/global/analysis/monthly_discharge/" # "/projects/wtrcycle/users/edwinhs/05min_runs_28_november_2014/multi_cores_non_natural_1960_to_2010/analysis/monthly_discharge/" # "/scratch/edwin/IWMI_run_20_nov/without_fossil_limit_with_pumping_limit_CRU/analysis/monthly_discharge/"
 cleanOutputDir          = True # option to clean analysisOutputDir 		
 
 # optional: PCR-GLOBWB output and analysis output folders are given in the system argument
@@ -30,17 +30,17 @@ cleanOutputDir          = True # option to clean analysisOutputDir
     #~ globalAnalysisOutputDir    = str(sys.argv[2])
 
 # time range for analyses
-startDate = None # "1960-01-31" #YYYY-MM-DD # None 
+startDate = None # "1958-01-31" #YYYY-MM-DD # None 
 endDate   = None # "2010-12-31" #YYYY-MM-DD # None 
 
 # directory for GRDC files:
-globalDirectoryGRDC = "/projects/wtrcycle/users/edwinhs/observation_data/grdc_monthly_data_splitted/" # "/scratch/edwin/observation_data/grdc_monthly_data_splitted/"
+globalDirectoryGRDC    = "/scratch-shared/edwinhs/observation_data/grdc_monthly_data_splitted/"                          # "/scratch/edwin/observation_data/grdc_monthly_data_splitted/"
 # This directory will contain several sub-folders (18 sub-folders per 10 Jun 2014)
 
-#~ # for 5min results of PCR-GLOBWB 2.0
-globalCloneMapFileName = "/projects/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"        # "/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
-lddMapFileName         = "/projects/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"        # "/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
-cellAreaMapFileName    = "/projects/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map" # "/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map"
+# for 5min results of PCR-GLOBWB 2.0
+globalCloneMapFileName = "/scratch-shared/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"        # "/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
+lddMapFileName         = "/scratch-shared/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"        # "/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
+cellAreaMapFileName    = "/scratch-shared/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map" # "/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map"
 
 #~ # for 30min results (of PCR-GLOBWB 2.0)
 #~ globalCloneMapFileName = "/data/hydroworld/PCRGLOBWB20/input30min/global/Global_CloneMap_30min.map"

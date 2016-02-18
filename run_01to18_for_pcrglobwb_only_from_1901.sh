57426b447a82e82920d43accbe6945072f80f3b5
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-18 17:49:13 +0100

    Analyzing discharge for /projects/0/dfguu/users/edwin/05min_runs_november_2015_merged/pcrglobwb_only_from_1901/1901_to_2010/

--- run_01to18_for_pcrglobwb_only_from_1901.sh
+++ run_01to18_for_pcrglobwb_only_from_1901.sh
@@ -1,3 +1,5 @@
+
+
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 01 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 02 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 03 &
@@ -7,6 +9,9 @@ python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 06 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 07 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 08 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 09 &
+
+wait
+
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 10 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 11 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 12 &
@@ -16,6 +21,7 @@ python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 15 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 16 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 17 &
 python 0_main_analyze_discharge_for_pcrglobwb_only_from_1901.py 18 &
+
 wait
 
 # for runs at cartesius

0776231ed8dc600954217698b25a2478005aecf0
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-10-25 16:00:06 +0200

    Start run_01to18_for_pcrglobwb_4_land_covers_edwin_parameter_set_watch_kinematicwave.sh

--- dischargeGRDC.py
+++ dischargeGRDC.py
@@ -283,7 +283,8 @@ class DischargeEvaluation(object):
                             pcr.boolean(1))
         
         # expanding the point
-        point = pcr.windowmajority(point, self.cell_size_in_arc_degree * 5.0)
+        #~ point = pcr.windowmajority(point, self.cell_size_in_arc_degree * 5.0)   #
+        point = pcr.windowmajority(point, 2.5)                                     # 2.5 degree
         point = pcr.ifthen(catchmentAreaAll > 0, point)
         point = pcr.boolean(point)
 

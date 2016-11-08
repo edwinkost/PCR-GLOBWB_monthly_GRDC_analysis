5e6d32de63f871f64210ba7159b799895da91fa1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2016-11-08 22:27:46 +0100

    Start run_01to18_for_pcrglobwb_4_land_covers_edwin_parameter_set_watch_accutraveltime.sh

--- 0_main_analyze_discharge_flexible.py
+++ 0_main_analyze_discharge_flexible.py
@@ -72,8 +72,8 @@ def main():
     
     # start date and end dates
     try: 
-        if str(argument[4]) != "default": endDate   = str(argument[4])
-        if str(argument[5]) != "default": startDate = str(argument[5])
+        if argument[4] != "default": endDate   = str(argument[4])
+        if argument[5] != "default": startDate = str(argument[5])
     except:
         pass
 

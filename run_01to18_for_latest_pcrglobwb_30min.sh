7b16c64127b8fe0dc6a20142a9953c00d91ae03d
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-09-05 06:08:16 +0200

    Check the PCR-GLOBWB results at 30 arcmin resolution.

--- run_01to18_for_latest_pcrglobwb_30min.sh
+++ run_01to18_for_latest_pcrglobwb_30min.sh
@@ -7,25 +7,25 @@ str_date=1958-01-31
 end_date=2015-12-31
 
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 01 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 02 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 03 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 04 $str_date $end_date &
-wait
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 05 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 06 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 07 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 08 $str_date $end_date &
-wait
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 09 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 10 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 11 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 12 $str_date $end_date &
-wait
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 13 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 14 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 15 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 16 $str_date $end_date &
-wait
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 17 $str_date $end_date &
-python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 18 $str_date $end_date &
-wait
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 02 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 03 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 04 $str_date $end_date &
+#~ wait
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 05 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 06 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 07 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 08 $str_date $end_date &
+#~ wait
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 09 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 10 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 11 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 12 $str_date $end_date &
+#~ wait
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 13 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 14 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 15 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 16 $str_date $end_date &
+#~ wait
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 17 $str_date $end_date &
+#~ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 18 $str_date $end_date &
+#~ wait

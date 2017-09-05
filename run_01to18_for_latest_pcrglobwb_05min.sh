43961668c7520fbadcecaba3098f3c3606952596
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-09-05 06:44:49 +0200

    Check the PCR-GLOBWB results at 5 arcmin resolution.

--- run_01to18_for_latest_pcrglobwb_05min.sh
+++ run_01to18_for_latest_pcrglobwb_05min.sh
@@ -1,7 +1,7 @@
 
 python_script_file_used="0_main_analyze_discharge_flexible_05min.py" 
 pcrglobwb_output_folder="/scratch-shared/edwinhs/runs_2017_july_aug_finalizing_4LCs/05min_runs/05min_runs_4LCs_accutraveltime_cru-forcing_1958-2015/non-natural_starting_from_1958/global/netcdf/"
-global_analysis_out_dir="/scratch-shared/edwinhs/runs_2017_july_aug_finalizing_4LCs/05min_runs/05min_runs_4LCs_accutraveltime_cru-forcing_1958-2015/non-natural/analysis/monthly_discharge_analysis_1985-2015/"
+global_analysis_out_dir="/scratch-shared/edwinhs/runs_2017_july_aug_finalizing_4LCs/05min_runs/05min_runs_4LCs_accutraveltime_cru-forcing_1958-2015/analysis/non-natural_monthly_discharge_analysis_1958-2015/"
 str_date=1958-01-31
 end_date=2015-12-31
 # Note that the GRDC observation data are not available until 2015 (requested by Edwin on 23 April 2014)                              
--- run_01to18_for_latest_pcrglobwb_30min.sh
+++ run_01to18_for_latest_pcrglobwb_30min.sh
@@ -1,7 +1,7 @@
 
 python_script_file_used="0_main_analyze_discharge_flexible_30min.py" 
 pcrglobwb_output_folder="/scratch-shared/edwinhs/runs_2017_finalizing_4LCs/30min/4LCs_accutraveltime_cru-forcing_latest_version/non-natural/begin_from_1958/netcdf/"
-global_analysis_out_dir="/scratch-shared/edwinhs/runs_2017_finalizing_4LCs/30min/4LCs_accutraveltime_cru-forcing_latest_version/non-natural/analysis/monthly_discharge_analysis_1985-2015/"
+global_analysis_out_dir="/scratch-shared/edwinhs/runs_2017_finalizing_4LCs/30min/4LCs_accutraveltime_cru-forcing_latest_version/non-natural/analysis/monthly_discharge_analysis_1958-2015/"
 str_date=1958-01-31
 end_date=2015-12-31
 # Note that the GRDC observation data are not available until 2015 (requested by Edwin on 23 April 2014)                              
@@ -10,7 +10,6 @@ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_di
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 02 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 03 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 04 $str_date $end_date &
-wait
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 05 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 06 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 07 $str_date $end_date &
@@ -20,7 +19,6 @@ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_di
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 10 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 11 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 12 $str_date $end_date &
-wait
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 13 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 14 $str_date $end_date &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 15 $str_date $end_date &

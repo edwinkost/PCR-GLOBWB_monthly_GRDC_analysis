c8bbb1a2b45d88a27c0bac80fddbd9f2adf8f01d
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-08-28 14:37:07 +0200

    discharge validation for run_01to18_for_pcrglobwb_for_land-subsidence-model_runs_version_201908XX_historical_non-natural_gmd-paper-irrigated_areas.sh

--- run_01to18_for_pcrglobwb_for_land-subsidence-model_runs_version_201908XX_historical_non-natural_gmd-paper-irrigated_areas.sh
+++ run_01to18_for_pcrglobwb_for_land-subsidence-model_runs_version_201908XX_historical_non-natural_gmd-paper-irrigated_areas.sh
@@ -1,7 +1,7 @@
 
 python_script_file_used="0_main_analyze_discharge_flexible_05min.py" 
 pcrglobwb_output_folder="/scratch-shared/edwin/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_gmd-paper-irrigated_areas/merged_1958-2015/netcdf/"
-global_analysis_out_dir="/scratch-shared/edwin/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_gmd-paper-irrigated_areas/validation/discharge_analysis_1958-2015/"
+global_analysis_out_dir="/scratch-shared/edwin/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_gmd-paper-irrigated_areas/validation/edwin_monthly_discharge_analysis_1958-2015/"
 str_date=1958-01-31
 end_date=2015-12-31
 # Note that the GRDC observation data are not available until 2015 (requested by Edwin on 23 April 2014)                              

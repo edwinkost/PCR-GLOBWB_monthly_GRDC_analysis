23dc5efd76fb0c0d5a7c33b72166b40290496672
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-11-25 11:46:37 +0100

    check uly results

--- run_01to18_for_pcrglobwb_uly_06min.sh
+++ run_01to18_for_pcrglobwb_uly_06min.sh
@@ -1,19 +1,30 @@
 
 python_script_file_used="0_main_analyze_discharge_flexible_06min.py"
 
-pcrglobwb_output_folder="/scratch/shared/edwin/uly_6min_results/"
+pcrglobwb_output_folder="/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-11-25/ulysse-et0_gmd-lcv/merged_1981-2019/"
 
-global_analysis_out_dir="/scratch/shared/edwin/uly_6min_results/edwin_monthly_discharge_evaluation/"
+#~ edwinhs@tcn758.bullx:/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-11-25/ulysse-et0_gmd-lcv/merged_1981-2019$ pwd
+
+global_analysis_out_dir="/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-11-25/ulysse-et0_gmd-lcv/merged_1981-2019/edwin_monthly_discharge_evaluation/"
 
 str_date=1981-01-31
 end_date=2019-12-31
 
 # Note that the GRDC observation data are not available until 2015 (requested by Edwin on 23 April 2014)                              
-discharge_file_name="discharge_monthAvg_output_1981-01-31_to_2019-12-31_remap.nc"
+discharge_file_name="discharge_monthAvg_output_1981-01-31_to_2019-12-31.nc"
 
-globalclone="/scratch/shared/edwin/uly_6min_results/clone.map" 
-lddmap_file="/scratch/shared/edwin/uly_6min_results/lddsound_06min_version_202007XX_for_ulysses.map" 
-cellarea_m2="/scratch/shared/edwin/uly_6min_results/cdo_griddarea.map" 
+#~ globalclone="/scratch/shared/edwin/uly_6min_results/clone.map" 
+#~ lddmap_file="/scratch/shared/edwin/uly_6min_results/lddsound_06min_version_202007XX_for_ulysses.map" 
+#~ cellarea_m2="/scratch/shared/edwin/uly_6min_results/cdo_griddarea.map" 
+
+globalclone="/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-11-25/maps/maps_1400_rows/land_mask_only.map" 
+lddmap_file="/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-11-25/maps/maps_1400_rows/lddsound_06min_version_202007XX_for_ulysses.map" 
+cellarea_m2="/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-11-25/maps/maps_1400_rows/cellarea_1400rows.map" 
+
+#~ edwinhs@tcn758.bullx:/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-11-25/maps/maps_1400_rows$ ls -lah *.map
+#~ -rw-r--r-- 1 edwinhs edwinhs  20M Nov 25 11:32 cellarea_1400rows.map
+#~ -rw-r----- 1 edwinhs edwinhs 4.9M Nov 25 11:32 land_mask_only.map
+#~ -rw-r--r-- 1 edwinhs edwinhs 4.9M Nov 25 11:32 lddsound_06min_version_202007XX_for_ulysses.map
 
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 01 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 02 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &

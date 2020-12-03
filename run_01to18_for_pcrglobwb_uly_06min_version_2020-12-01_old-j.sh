13ff66f26ffefd6c24fc90f1ecccd5546a0af5b9
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-12-03 23:44:59 +0100

    runs with jobs

--- run_01to18_for_pcrglobwb_uly_06min_version_2020-12-01_old-j.sh
+++ run_01to18_for_pcrglobwb_uly_06min_version_2020-12-01_old-j.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 119:59:00
+#SBATCH -p normal
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=edwinkost@gmail.com
 
 python_script_file_used="0_main_analyze_discharge_flexible_06min.py"
 
 pcrglobwb_output_folder="/scratch-shared/edwinhs/discharge_evaluation_ulysses_2020-12-01/old-jgw_uly-et0_uly-lcv/merged_1981-1998/"
 
-global_analysis_out_dir=${pcrglobwb_output_folder}"/edwin_monthly_discharge_evaluation/"
+global_analysis_out_dir=${pcrglobwb_output_folder}"/edwin_monthly_discharge_evaluation_with_job/"
 
 #~ str_date=1981-01-31
 #~ end_date=2019-12-31
@@ -42,6 +52,8 @@ python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_di
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 08 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
 
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 09 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+
+wait
 
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 10 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
 python $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir 11 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &

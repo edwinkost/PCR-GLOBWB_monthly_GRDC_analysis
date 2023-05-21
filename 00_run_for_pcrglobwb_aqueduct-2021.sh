95f8dfae6790f60c2fa2ccdf5b07bcf302bbf44f
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-05-21 21:58:22 +0200

    rerun - test

--- 00_run_for_pcrglobwb_aqueduct-2021.sh
+++ 00_run_for_pcrglobwb_aqueduct-2021.sh
@@ -24,7 +24,7 @@ python_script_file_used="0_main_analyze_discharge_flexible_aqueduct-2021.py"
 
 pcrglobwb_output_folder="/scratch-shared/edwin/discharge_aqueduct_2021/"
 
-global_analysis_out_dir=${pcrglobwb_output_folder}"/validation_1979-2019_final_v20230521/"
+global_analysis_out_dir=${pcrglobwb_output_folder}"/validation_1979-2019_final_v20230521_test/"
 
 discharge_file_name="pcrglobwb_cmip6-isimip3-gswp3-w5e5_image-aqueduct_historical-reference_discharge_global_monthly-average_1960_2019_basetier1.nc"
 

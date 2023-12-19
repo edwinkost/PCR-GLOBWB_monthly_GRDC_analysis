090e075e4706357880d9fe579d7a6a03f146ab6d
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-12-19 12:45:00 +0100

    discharge analysis for ulysses

---
+++ 0000_main_analyze_discharge_flexible_ulysses-2023.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This is script for evaluating monthly simulated discharge to GRDC discharge data.
+
+import os
+import sys
+import glob
+
+import dischargeGRDC
+
+import logging
+from logger import Logger
+# get name for the logger
+logger = logging.getLogger("main_script")
+
+#~ # PCR-GLOBWB results: model output directory
+#~ pcrglobwb_output = {}
+#~ pcrglobwb_output["folder"]               = None                      # "/scratch-shared/edwinhs/runs_2017_finalizing_4LCs/30min/4LCs_accutraveltime_cru-forcing_latest_version/non-natural/begin_from_1958/netcdf/" # 
+#~ pcrglobwb_output["netcdf_file_name"]     = "discharge_monthAvg_output.nc" 
+#~ pcrglobwb_output["netcdf_variable_name"] = "discharge" 
+
+# PCR-GLOBWB results: model output directory
+pcrglobwb_output = {}
+# ~ pcrglobwb_output["folder"]           = "/scratch/depfg/sutan101/pcrglobwb_ulysses_reference_runs_version_2021-01-XX_b/1.50/" 
+pcrglobwb_output["folder"]               = None 
+pcrglobwb_output["netcdf_file_name"]     = "discharge_monthAvg_output_1981-2019.nc" 
+pcrglobwb_output["netcdf_variable_name"] = "discharge" 
+
+
+# output directory storing analysis results (results from this script)
+globalAnalysisOutputDir                  = "/scratch/depfg/sutan101/discharge_ulysses_validation/test/"
+cleanOutputDir                           = True                         # option to clean analysisOutputDir   # FIXME: This must always be True 		
+
+# directory for GRDC files:
+# ~ globalDirectoryGRDC = "/projects/0/dfguu/users/edwin/data/observation_data/grdc_monthly_data_splitted/"           # "/scratch/edwin/observation_data/grdc_monthly_data_splitted/"
+# ~ # This directory will contain several sub-folders (18 sub-folders per 10 Jun 2014)
+
+# ~ # - on snellius
+# ~ globalDirectoryGRDC = "/projects/0/dfguu/users/edwin/data/grdc_data_monthly_requested_on_2023013/txt_splitted/"
+
+# - on eejit
+globalDirectoryGRDC     = "/scratch/depfg/sutan101/data/grdc_data_monthly_requested_on_2023013/txt_splitted/"
+
+
+# for 5min results of PCR-GLOBWB 2.0
+#~ globalCloneMapFileName = "/projects/0/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"        # "/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
+#~ lddMapFileName         = "/projects/0/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"        # "/data/hydroworld/PCRGLOBWB20/input5min/routing/lddsound_05min.map"
+#~ cellAreaMapFileName    = "/projects/0/dfguu/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map" # "/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map"
+
+# ~ # for 5min results of PCR-GLOBWB 2.0
+# ~ globalCloneMapFileName = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/lddsound_05min_version_20210330.map"
+# ~ lddMapFileName         = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/lddsound_05min_version_20210330.map"
+# ~ cellAreaMapFileName    = "/projecs/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/cdo_gridarea_clone_global_05min_correct_lats.nc.map"
+
+# ~ # 6 arcmin ulysses
+# ~ lddMapFileName         = "/scratch/depfg/sutan101/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/lddsound_06min_version_202007XX_for_ulysses.map"
+# ~ globalCloneMapFileName = "/scratch/depfg/sutan101/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/lddsound_06min_version_202007XX_for_ulysses.map"
+# ~ cellAreaMapFileName    = "/scratch/depfg/sutan101/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/cellarea.map"
+
+# ~ # 6 arcmin ulysses
+# ~ lddMapFileName         = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/lddsound_06min_version_202007XX_for_ulysses.map"
+# ~ globalCloneMapFileName = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/lddsound_06min_version_202007XX_for_ulysses.map"
+# ~ cellAreaMapFileName    = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/cellarea.map"
+
+lddMapFileName         = None
+globalCloneMapFileName = None
+cellAreaMapFileName    = None
+
+
+# the following is needed for evaluating model results with 5 arcmin resolution (but optional)
+catchmentClassFileName = None 
+
+def main():
+
+    # get the argument: 
+    argument = sys.argv
+    print(argument)
+    
+    # PCR-GLOBWB results: model output directory
+    pcrglobwb_output["folder"] = str(argument[1])
+    
+    # output directory storing analysis results (results from this script)
+    globalAnalysisOutputDir    = str(argument[2])
+    
+    # sub_folder to split/parallelize the analyses
+    sub_folder                 = str(argument[3])
+    
+    # start date and end dates for analyses
+    startDate = None # "1979-01-31" # None # "1958-01-31" #YYYY-MM-DD # None 
+    endDate   = None # "2019-12-31" # None # "2010-12-31" #YYYY-MM-DD # None 
+    try: 
+        if argument[5] != "default": endDate   = str(argument[5])
+        if argument[4] != "default": startDate = str(argument[4])
+    except:
+        pass
+
+    # netcdf file name and variable name
+    try: 
+        pcrglobwb_output["netcdf_file_name"] = str(argument[6])
+    except:
+        pass
+
+    # netcdf variable name
+    try: 
+        pcrglobwb_output["netcdf_variable_name"] = str(argument[7])
+    except:
+        pass
+    
+    # clone map, ldd and cell area 
+    globalCloneMapFileName = str(argument[8])
+    lddMapFileName         = str(argument[9])
+    cellAreaMapFileName    = str(argument[10])
+    
+    # sub-folders of the analysis output and GRDC folders 
+    analysisOutputDir = globalAnalysisOutputDir + "/" + sub_folder + "/"
+    directoryGRDC     = globalDirectoryGRDC + "/" + sub_folder + "/"
+    
+    # make analysisOutputDir
+    try:
+        os.makedirs(analysisOutputDir) 
+    except:
+        if cleanOutputDir == True: os.system('rm -r '+analysisOutputDir+"/*") 
+    #
+    # temporary directory (note that it is NOT a good idea to store temporary files in the memory (/dev/shm))
+    temporary_directory = analysisOutputDir + "/tmp/"
+    try:
+        os.makedirs(temporary_directory) 
+    except:
+        os.system('rm -r '+temporary_directory+"/*") # make sure that temporary directory is clean 
+
+    # logger object
+    logger = Logger(analysisOutputDir)
+    
+    # get GRDC attributes of all stations:
+    dischargeEvaluation = dischargeGRDC.DischargeEvaluation(pcrglobwb_output["folder"],\
+                                                            startDate,endDate,temporary_directory)
+    dischargeEvaluation.get_grdc_attributes(directoryGRDC)
+
+    # evaluate model results
+    dischargeEvaluation.evaluateAllModelResults(globalCloneMapFileName,\
+                                                catchmentClassFileName,\
+                                                lddMapFileName,\
+                                                cellAreaMapFileName,\
+                                                pcrglobwb_output,\
+                                                analysisOutputDir)  
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ 0000_run_for_ulysses-2023_1981-1995.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 32
+
+#SBATCH -t 119:59:59
+
+#~ #SBATCH -t 0:59:59
+
+# eejit
+#SBATCH -p defq
+
+#~ #SBATCH -p genoa
+
+#~ #SBATCH --exclusive
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=edwinkost@gmail.com
+
+#SBATCH -J grdc_val
+## this is the job name
+
+# exporting some variables
+#SBATCH --export DISCHARGE_OUTPUT_FOLDER=${DISCHARGE_OUTPUT_FOLDER},DISCHARGE_FILENAME=${DISCHARGE_FILENAME},ANALYSIS_OUTPUT_FOLDER=${ANALYSIS_OUTPUT_FOLDER}
+
+
+# -- eejit (Note: You may have problems while plotting hydrographs with ggplot2) 
+# load pcraster etc
+. /eejit/home/sutan101/load_default.sh
+# load R
+. /eejit/home/sutan101/load_R.sh
+
+
+#~ # -- snellius genoa
+#~ module load 2022
+#~ module load R/4.2.1-foss-2022a
+#~ module load Miniconda3/4.12.0
+#~ unset PYTHONPATH
+#~ source activate /home/hydrowld/.conda/envs/pcrglobwb_python3_2023-10-31
+
+
+
+python_script_file_used="0000_main_analyze_discharge_flexible_ulysses-2023.py"
+
+#~ discharge_output_folder="/scratch/depfg/sutan101/pcrglobwb_ulysses_2023-12-XX_validation/mhm/"
+discharge_output_folder=${DISCHARGE_OUTPUT_FOLDER}
+
+#~ discharge_file_name="discharge_monthAvg_output_mhm_1981-1995.nc"
+discharge_file_name=${DISCHARGE_FILENAME}
+
+# analysis output folder
+global_analysis_out_dir=${ANALYSIS_OUTPUT_FOLDER}
+
+str_date=1981-01-31
+end_date=1995-12-31
+#~ str_date=1981-01-31
+#~ end_date=2019-12-31
+#~ str_date=1981-01-31
+#~ end_date=1998-12-31
+
+# clone, ldd and cellare file used for 6 arcmin ulysses runs
+#
+# - on eejit
+globalclone="/scratch/depfg/sutan101/pcrglobwb_input_ulysses_v202312XX/develop_edwin/lddsound_06min_version_202007XX_for_ulysses.map"
+lddmap_file="/scratch/depfg/sutan101/pcrglobwb_input_ulysses_v202312XX/develop_edwin/lddsound_06min_version_202007XX_for_ulysses.map"
+cellarea_m2="/scratch/depfg/sutan101/pcrglobwb_input_ulysses_v202312XX/develop_edwin/cellarea.map"
+#
+#~ # - on snellius
+#~ globalclone="/projects/0/dfguu/users/edwin/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/lddsound_06min_version_202007XX_for_ulysses.map"
+#~ lddmap_file="/projects/0/dfguu/users/edwin/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/lddsound_06min_version_202007XX_for_ulysses.map"
+#~ cellarea_m2="/projects/0/dfguu/users/edwin/data/pcrglobwb_input_ulysses/pcrglobwb_ulysses_input_files_v2021-01-03/cellarea.map"
+
+
+#~ # for testing
+#~ python3 $python_script_file_used $pcrglobwb_output_folder $global_analysis_out_dir test $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+
+# actual runs
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 01 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 02 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 03 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 04 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 05 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 06 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 07 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 08 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 09 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 10 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 11 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 12 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 13 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 14 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 15 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 16 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 17 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 18 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 19 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 20 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 21 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 22 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 23 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 24 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 25 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 26 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 27 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 28 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 29 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+python3 $python_script_file_used $discharge_output_folder $global_analysis_out_dir 30 $str_date $end_date $discharge_file_name discharge ${globalclone} ${lddmap_file} ${cellarea_m2} &
+
+wait

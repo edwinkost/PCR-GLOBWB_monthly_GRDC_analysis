071a06bb7f7495692331f922514085b17317a592
root <root@puppylinux_vgen>
2026-06-16 15:11:46 +0200

    submit slurm jobs on eejit

--- dischargeGRDC.py
+++ dischargeGRDC.py
@@ -481,6 +481,7 @@ class DischargeEvaluation(object):
             
             # ~ f = nc.Dataset(ncFile)
             f = nc.Dataset("/scratch/depfg/sutan101/watersis_runs_may_2026/global_6min_with_watersis_forcing_with_parallel_v20260604/pgb_6min/begin_from_1970/global/netcdf/merged/discharge_monthAvg_output_1970-2019_global6min_v20260604.nc")
+            print("pietje")
             
             # ~ if ncFile in filecache.keys():
                 # ~ f = filecache[ncFile]
